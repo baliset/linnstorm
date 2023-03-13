470b29330c95a30a81fb81d0723837ea2ebeef9a
Haim Zamir <hzamir@gmail.com>
2023-03-13 15:25:44 -0400

    use patchNameRenderer in columndefs.js

--- src/xform/columndefs.js
+++ src/xform/columndefs.js
@@ -215,8 +215,8 @@ const linnPropColumns = [
 
 const patchColumns = [
     {f:'sel', maxWidth:50, cellRenderer: 'checkboxRenderer', floatingFilter:false},
-    {f: 'updated', maxWidth:140, comparator:numberSort, valueFormatter:vfDateTime,  cellRenderer: 'updatedRenderer'},
-    {f: 'name', maxWidth:140, editable: true, cellEditor: 'patchNameEditor', floatingFilter: true, floatingFilterComponentParams: { suppressFilterButton: true }},
+    {f: 'updated', maxWidth:140, comparator:numberSort, cellRenderer: 'updatedRenderer'},
+    {f: 'name', maxWidth:140, editable: true, cellEditor: 'patchNameEditor', cellRenderer: 'patchNameRenderer', floatingFilter: true, floatingFilterComponentParams: { suppressFilterButton: true }},
     {f: 'comments', editable: true, cellEditor: 'patchCommentEditor', floatingFilter: true, floatingFilterComponentParams: { suppressFilterButton: true }},
 ].map(o=>({...o,  suppressMenu: true, }));
 
