d6bf8ce4515e9c44da6461953360c1cb7ac6c720
Haim Zamir <hzamir@gmail.com>
2023-03-14 11:23:28 -0400

    expose Swatch and colors

--- src/agstuff/LinnParamRenderer.jsx
+++ src/agstuff/LinnParamRenderer.jsx
@@ -1,16 +1,17 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React from 'react';
 import {vfExpander, vfParamType} from "../linnutils/linn-expansion";
 import styled from "styled-components";
 
-const colors = [
+export const colors = [
 '#000', '#f00','#ffd700',
 '#0f0', '#0ff','#00f',
 '#f0f', '#888','#fff',
 '#ffa500', '#df0','#faa'];
 
-const Swatch = styled.div`
+export const Swatch = styled.div`
     display: inline-block;
-    width: 100%;
+    min-width: 100%;
+    min-height: 100%;
     background-color: ${p => colors[p.color]};
     color: ${p => (p.color === 0||p.color === 5)? 'white':'black'};
 `;
